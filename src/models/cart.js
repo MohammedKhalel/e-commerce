5dc45eb4ba839eaf9c9adab78fcca26f59fca5ed
Mohamed Khalil <khalelz123z@gmail.com>
2024-03-18 16:50:26 +0200

    first commit

---
+++ src/models/cart.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const Product = require('./product')
+const cartSchema = new mongoose.Schema({
+    products:[
+        {
+            productId:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Product',
+                // required:true
+            },
+            quantity:{
+                type:Number,
+                default:1
+            }
+        }
+    ],
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    }
+  
+},{timestamps:true})
+
+cartSchema.pre('save',async function (){})
+
+// cartSchema.methods.FindProductId = async(productID)=>{
+// const product = await Cart.findOne({'products.productID':productID})
+// if(!product){
+//     throw new Error("Product not found")
+// }
+// return product
+// }
+
+const Cart = mongoose.model('Cart',cartSchema) 
+
+module.exports = Cart
